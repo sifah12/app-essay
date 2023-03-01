20c6978449ec1edf190512a472391270760ef2ba
sifah12 <sifaalliance2020@gmail.com>
2023-03-01 14:52:48 +0000

    setupfile

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 // mardi le 28/03 how to write name and campany in browser
 
-
 // import { Component } from "react";
 // import "./App.css";
-
 
 // class App extends Component {
 //   constructor() {
@@ -21,7 +19,7 @@
 //   render() {
 //     return (
 //       <div classsName="App">
-//         <h1>{this.state.name}</h1> 
+//         <h1>{this.state.name}</h1>
 //         <p>{this.state.campany}</p>
 //         <button onClick={this.messageapp}>click</button>
 
@@ -32,10 +30,8 @@
 
 // export default App;
 
-
 // import { Component } from "react";
 // import "./App.css";
-
 
 // class App extends Component {
 // constructor(){
@@ -44,7 +40,6 @@
 //   value:"v",
 //   github:""
 //   }
-
 
 // }
 // message =(event)=>{
@@ -57,12 +52,11 @@
 //   return(
 
 // <div className="logos">
- 
 
 // {/* {this.state.github} */}
 // <input type="text" onChange={this.message} />
 // <p>{this.state.value}</p>
-  
+
 //   <button onClick={this.message}>submit</button>
 // </div>
 
@@ -72,77 +66,10 @@
 // }
 
 //   export default App;
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
 
-class App extends Component {
-  constructor(){
-    super()
-    this.state={
-      data:[],
-      loading:true,
-      searchQerry:''
-    }
-  }
-  
-   async componentDidMount(){
-    const result=await fetch("https://api.openweathermap.org/data/2.5/forecast?q=kigali&appid=1383c1ef39cccbbc106d269850cfc481")
-    const mydata=await result.json()
-    this.setState({data:mydata.list,loading:false})
-   }
-
-   search=(event)=>{
-const searchQerry= event.target.value
-console.log(searchQerry)
-console.log(this.state)
-this.setState({searchQerry});
-}
-   
-
-  
+export default class App extends Component {
   render() {
-    let filteredList=null
-    filteredList= this.state.data.filter((filterData)=>(
-      filterData.weather[0].main.toLowerCase().includes(this.state.searchQerry.toLowerCase())
-    ))
-
-    let displayContent=null
-    if(this.state.loading)
-    {
-      displayContent=<div>
-        <h1>loading..........</h1>
-      </div>
-    }
-    else if(filteredList.length>0)
-    {
-    displayContent=  
-      (
-      <div>
-        
-        {filteredList.map((mydata)=>(
-      <h1>{mydata.weather[0].main}</h1>
-      
-      
-        ))}
-      </div>
-      )
-    }
-    else{
-      displayContent=<div>
-        <p style={{color:"white"}}>no result found</p>
-      </div>
-    }
-    return (
-    
-    
-      <div className="logos">
-        <input type onChange={this.search} className="myths"></input>
-        
-     <div className='nav' >{displayContent}</div>
-      </div>
-    );
-     }
+    return <div>App</div>;
   }
-
-
-export default App;
+}
